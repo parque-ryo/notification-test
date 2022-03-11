76e5c99bc452c50831725782c2a0de88ced5c779
ro <ro.michi1129@gmail.com>
2022-03-11 14:06:12 +0900

    Delete firebase dependencies

--- src/components/domain/auth/SignOutButton.tsx
+++ src/components/domain/auth/SignOutButton.tsx
@@ -1,19 +1,15 @@
-import { useAuth } from "~/lib/firebase";
+// import { useAuth } from '~/lib/firebase';
 
 type Props = {};
 
 export const SignOutButton = (props: Props) => {
   const handleClick = () => {
-    const auth = useAuth();
-    auth.signOut();
+    // const auth = useAuth();
+    // auth.signOut();
   };
 
   return (
-    <button
-      onClick={handleClick}
-      type="button"
-      className="btn normal-case"
-    >
+    <button onClick={handleClick} type="button" className="btn normal-case">
       Sign Out
     </button>
   );
